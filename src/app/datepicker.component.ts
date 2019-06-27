6256ecf58c7a32123d1a7d58b51c4db3e60b279a
Corralini <acorralfdez@gmail.com>
2019-06-27 13:02:19 +0200

    Minimalist

--- src/app/datepicker.component.ts
+++ src/app/datepicker.component.ts
@@ -6,14 +6,6 @@ import {NgbDateStruct, NgbCalendar} from '@ng-bootstrap/ng-bootstrap';
   templateUrl: './datepicker.html'
 })
 export class NgbdDatepickerBasic {
-
   model: NgbDateStruct;
   date: {year: number, month: number};
-
-  constructor(private calendar: NgbCalendar) {
-  }
-
-  selectToday() {
-    this.model = this.calendar.getToday();
-  }
 }
